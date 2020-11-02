d96a3c3c550ebb8cc25929ef4dd8d6d475e88b5b
Omer Lerner <omerler@post.bgu.ac.il>
2020-11-02 12:34:18 +0200

    Added buttons, results display on screen instead of console, game goes up to 5 points then immediately resets

--- scripts.js
+++ scripts.js
@@ -56,39 +56,51 @@ function computerPlay() //Randomly generates the CPU's selection
             return "error";
     }
 }
-function game() //Simulates a game of X rounds, X is decided by the value of "amountOfGames"
+function reset()
 {
-    let playerSelection, computerSelection,gameResult, playerScore=0, computerScore=0, amountOfGames;
-    amountOfGames=window.prompt("How many games would you like to play?"); //Choose the amount of games you want to play vs the CPU, rounds up decimal numbers
-    amountOfGames=parseInt(amountOfGames,10);
-    while (isNaN(amountOfGames) | amountOfGames<0)
+    playerScore=0; computerScore=0; outputText.textContent="";
+    pScore.textContent=playerScore;
+    cScore.textContent=computerScore;
+}
+function game(playerSelection) //Simulates a game of X rounds, X is decided by the value of "amountOfGames"
+{
+    if (playerScore==5 || computerScore==5)
     {
-        amountOfGames=window.prompt("Enter a valid amount of games.");
-        amountOfGames=parseInt(amountOfGames,10);
+        reset();
     }
-    for (i=0; i<=amountOfGames; i++)
-    {
-    playerSelection = window.prompt("Choose Rock, Paper or Scissors! ");
-    playerSelection = playerSelection.toLowerCase(); 
-    console.log(playerSelection);
-    while (playerSelection!="rock" && playerSelection!="scissors" && playerSelection!="paper") 
-    {
-        playerSelection = window.prompt("Type a valid input.");
-        playerSelection = playerSelection.toLowerCase();
-    }
+    let computerSelection,gameResult, amountOfGames;
     computerSelection = computerPlay(); //Randomly generates Rock, Paper or Scissors for the computer
     gameResult = playRound(playerSelection, computerSelection);// Calculates the winner of the game
     if (gameResult.search("win")!=-1)// If "Win" appears in the returned string, player has won
+    {
         playerScore++;
-    else if(gameResult.search("lose")!=-1)// If "Lose" appears in the returned string, computer has won. Else, it's a tie
-        computerScore++;
-    console.log(gameResult+" "+scoreOutput(playerScore,computerScore)); //Outputs the winner and the current score
+        pScore.textContent=playerScore;
     }
-    if (playerScore>computerScore)
-        console.log("You are the winner! The final score is "+playerScore+"-"+computerScore);
-    else if (computerScore > playerScore)
-        console.log("You lose! The final score is "+computerScore+"-"+playerScore);
+    else if(gameResult.search("lose")!=-1)// If "Lose" appears in the returned string, computer has won. Else, it's a tie
+    {
+        computerScore++;
+        cScore.textContent=computerScore;
+    }
+    console.log(gameResult+" "+scoreOutput(playerScore,computerScore)); //Outputs the winner and the current score
+    if (playerScore==5)
+        outputText.textContent="You are the winner! The final score is "+playerScore+"-"+computerScore;
+    else if (computerScore==5)
+        outputText.textContent="You lose! The final score is "+computerScore+"-"+playerScore;
     else
-        console.log("Tie game! The final score is "+playerScore+"-"+computerScore);
+        outputText.textContent=gameResult;
+
 }
-game();
+const container= document.querySelector('#container');
+const buttons = container.querySelectorAll('button');
+let playerScore=0, computerScore=0;
+const pScore = document.querySelector('#player-score'), cScore = document.querySelector('#computer-score'), outputText=document.querySelector('#output-text')
+pScore.textContent=playerScore; cScore.textContent=computerScore;             
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      game(button.id);
+    });
+  });
+const resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click',() =>{
+         reset();
+     });
